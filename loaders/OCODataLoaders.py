b564cce46f0dcc4d7c460b4409eae894417debd7
William Keely <williamkeely@gmail.com>
2024-07-02 12:28:20 -0500

    simplfied implmentation

--- loaders/OCODataLoaders.py
+++ loaders/OCODataLoaders.py
@@ -66,193 +66,123 @@ class RetrievalDataSet(Dataset):
         self.transform = transform
         self.normalize_file = normalize_file
         self.use_convolutions = use_convolutions
-        
-        # Load data based on the file paths
         self.X, self.y = self.load_data()
 
     def load_data(self):
-        sco2 = []
-        wco2 = []
-        o2 = []
-        psurf_prior = []
-        geometry = []
-        y = []
-
-        
+        data = { 'sco2': [], 'wco2': [], 'o2': [], 'psurf_prior': [], 'geometry': [], 'y': [] }
         for file_path in self.files:
             try:
                 with open(file_path, 'rb') as file:
                     file_data = pickle.load(file)
-                    
-                    if self.train:
-                        print("shape of sco2 : ", file_data['sco2_band_no_eof'].shape)
-                        print("shape of wco2 : ", file_data['wco2_band_no_eof'].shape)
-                        print("shape of o2 : ", file_data['o2_band_no_eof'].shape)
-                        sco2.append(file_data['sco2_band_no_eof'])
-                        wco2.append(file_data['wco2_band_no_eof'])
-                        o2.append(file_data['o2_band_no_eof'])
-                    else:
-                        sco2.append(file_data['sco2_band_obs'])
-                        wco2.append(file_data['wco2_band_obs'])
-                        o2.append(file_data['o2_band_obs'])
-                    psurf = file_data['states'][:,0]
-                    geom = file_data['states'][:,11:15]
-                    xco2 = file_data['states'][:, 20]
-                    psurf = np.array(psurf)
-                    geom = np.array(geom)
-                    xco2 = np.array(xco2)
-                    psurf = psurf.reshape(-1, 1)
-                    geom = geom.reshape(-1, 4)
-                    xco2 = xco2.reshape(-1, 1)
-                    # check that psurf, geom and xco2 are of shape (n_samples, 1)
+                    for key in ['sco2', 'wco2', 'o2']:
+                        band_key = f'{key}_band_no_eof' if self.train else f'{key}_band_obs'
+                        data[key].append(file_data[band_key])
 
-                    psurf_prior.append(psurf)
-                    geometry.append(geom)
-                    y.append(xco2)
+                    data['psurf_prior'].append(file_data['states'][:, 0].reshape(-1, 1))
+                    data['geometry'].append(file_data['states'][:, 11:15])
+                    data['y'].append(file_data['states'][:, 20].reshape(-1, 1))
 
-                    print('xco2 shape : ', xco2.shape)
-                    print('xco2 values : ', xco2)
-
-                    
             except (FileNotFoundError, KeyError, pickle.UnpicklingError) as e:
                 print(f"Error loading file {file_path}: {e}")
-
+        for key in data:
+            data[key] = np.vstack(data[key])
+            # to torch tensor
+            data[key] = torch.tensor(data[key], dtype=torch.float32)
+            if len(data[key].shape) == 3:
+                data[key] = data[key].squeeze(1)
 
         
-        if self.normalize:
-            if not self.train:
-                # make everything a torch tensor
-                sco2 = np.vstack(sco2)
-                wco2 = np.vstack(wco2)
-                o2 = np.vstack(o2)
-                psurf_prior = np.vstack(psurf_prior)
-                geometry = np.vstack(geometry)
-                y = np.vstack(y)
+        mask = torch.isnan(data['sco2']).any(axis=1) | torch.isnan(data['wco2']).any(axis=1) | \
+               torch.isnan(data['o2']).any(axis=1) | torch.isnan(data['psurf_prior']).any(axis=1) | \
+               torch.isnan(data['geometry']).any(axis=1) | torch.isnan(data['y']).any(axis=1)
+        
+        for key in data:
+            data[key] = data[key][~mask]
+            if self.normalize:
+                if self.train:
+                    self.calculate_and_save_normalization_params(data)
+                    self.apply_normalization(data)
+                else:
+                    self.apply_normalization(data)
 
-                sco2 = torch.tensor(sco2, dtype=torch.float32)
-                wco2 = torch.tensor(wco2, dtype=torch.float32)
-                o2 = torch.tensor(o2, dtype=torch.float32)
-                psurf_prior = torch.tensor(psurf_prior, dtype=torch.float32)
-                geometry = torch.tensor(geometry, dtype=torch.float32)
-                y = torch.tensor(y, dtype=torch.float32)
+        X = torch.cat([data['o2'], data['wco2'], data['sco2'], data['psurf_prior'], data['geometry']], axis=1)
+        y = data['y']
 
-                with open(self.normalize_file, 'rb') as file:
-                    norm_params = pickle.load(file)
-                sco2 = (sco2 - norm_params['sco2_mean']) / norm_params['sco2_std']
-                wco2 = (wco2 - norm_params['wco2_mean']) / norm_params['wco2_std']
-                o2 = (o2 - norm_params['o2_mean']) / norm_params['o2_std']
-                psurf_prior = (psurf_prior - norm_params['psurf_prior_mean']) / norm_params['psurf_prior_std']
-                geometry = (geometry - norm_params['geometry_mean']) / norm_params['geometry_std']
-                y = (y - norm_params['targets_mean']) / norm_params['targets_std']
-                # squeez the data
-                if len(sco2.shape) == 3:
-                    sco2 = sco2.squeeze(1)
-                    wco2 = wco2.squeeze(1)
-                    o2 = o2.squeeze(1)
-                    psurf_prior = psurf_prior.squeeze(1)
-                    geometry = geometry.squeeze(1)
-                    y = y.squeeze(1)
-                # remove nans
-                mask = torch.isnan(sco2).any(axis=1) | torch.isnan(wco2).any(axis=1) | torch.isnan(o2).any(axis=1) | torch.isnan(y).any(axis=1) | torch.isnan(psurf_prior).any(axis=1) | torch.isnan(geometry).any(axis=1)
-                sco2 = sco2[~mask]
-                wco2 = wco2[~mask]
-                o2 = o2[~mask]
-                psurf_prior = psurf_prior[~mask]
-                geometry = geometry[~mask]
-                y = y[~mask]
-                # concat sco2, wco2, and o2
-                X = torch.cat([o2, wco2, sco2, psurf_prior, geometry], axis=1)
-
-                # return the last 1000 samples for testing
-
-
-            else:
-                sco2 = np.vstack(sco2)
-                wco2 = np.vstack(wco2)
-                o2 = np.vstack(o2)
-                psurf_prior = np.vstack(psurf_prior)
-                geometry = np.vstack(geometry)
-                y = np.vstack(y)
-
-                print("shape of sco2 : ", sco2.shape)
-                print("shape of wco2 : ", wco2.shape)
-                print("shape of o2 : ", o2.shape)
-                # make everything a torch tensor
-                sco2 = torch.tensor(sco2, dtype=torch.float32)
-                wco2 = torch.tensor(wco2, dtype=torch.float32)
-                o2 = torch.tensor(o2, dtype=torch.float32)
-                psurf_prior = torch.tensor(psurf_prior, dtype=torch.float32)
-                geometry = torch.tensor(geometry, dtype=torch.float32)
-                y = torch.tensor(y, dtype=torch.float32)
-                # if any shape length is 3 (i.e. [n_samples, 1, n_features]), then squeeze
-                if len(sco2.shape) == 3:
-                    sco2 = sco2.squeeze(1)
-                    wco2 = wco2.squeeze(1)
-                    o2 = o2.squeeze(1)
-                    psurf_prior = psurf_prior.squeeze(1)
-                    geometry = geometry.squeeze(1)
-                    y = y.squeeze(1)
-                # remove nans
-                mask = torch.isnan(sco2).any(axis=1) | torch.isnan(wco2).any(axis=1) | torch.isnan(o2).any(axis=1) | torch.isnan(y).any(axis=1) | torch.isnan(psurf_prior).any(axis=1) | torch.isnan(geometry).any(axis=1)
-                sco2 = sco2[~mask]
-                wco2 = wco2[~mask]
-                o2 = o2[~mask]
-                psurf_prior = psurf_prior[~mask]
-                geometry = geometry[~mask]
-                y = y[~mask]
-                # calculate the mean and std for normalization
-                sco2_mean = torch.mean(sco2)
-                sco2_std = torch.std(sco2)
-                wco2_mean = torch.mean(wco2)
-                wco2_std = torch.std(wco2)
-                o2_mean = torch.mean(o2)
-                o2_std = torch.std(o2)
-                psurf_prior_mean = torch.mean(psurf_prior)
-                psurf_prior_std = torch.std(psurf_prior)
-                geometry_mean = torch.mean(geometry)
-                geometry_std = torch.std(geometry)
-                targets_mean = torch.mean(y)
-                targets_std = torch.std(y)
-                sco2 = (sco2 - sco2_mean) / sco2_std
-                wco2 = (wco2 - wco2_mean) / wco2_std
-                o2 = (o2 - o2_mean) / o2_std
-                psurf_prior = (psurf_prior - psurf_prior_mean) / psurf_prior_std
-                geometry = (geometry - geometry_mean) / geometry_std
-                y = (y - targets_mean) / targets_std
-                # concat sco2, wco2, and o2
-                X = torch.cat([o2, wco2, sco2, psurf_prior, geometry], axis=1)
-
-                with open(self.normalize_file, 'wb') as file:
-                    pickle.dump({'sco2_mean': sco2_mean, 'sco2_std': sco2_std,
-                                 'wco2_mean': wco2_mean, 'wco2_std': wco2_std,
-                                 'o2_mean': o2_mean, 'o2_std': o2_std,
-                                    'psurf_prior_mean': psurf_prior_mean, 'psurf_prior_std': psurf_prior_std,
-                                    'geometry_mean': geometry_mean, 'geometry_std': geometry_std,
-                                 'targets_mean': targets_mean, 'targets_std': targets_std}, file)
-                                   
-
-
-        print(X.shape)
-        assert X.shape[0] == y.shape[0], "Data lengths do not match"
-        # y = y.unsqueeze(-1)
-        # if use_convolutions is True, reshape the data to [1, number of samples, number of features]
         if self.use_convolutions:
             X = X.unsqueeze(0)
             y = y.unsqueeze(0)
 
-
-    
         return X, y
 
-    def __len__(self) -> int:
+    def apply_normalization(self, data):
+        with open(self.normalize_file, 'rb') as file:
+            norm_params = pickle.load(file)
+        for key in ['sco2', 'wco2', 'o2', 'psurf_prior', 'geometry', 'y']:
+            mean, std = norm_params[f'{key}_mean'], norm_params[f'{key}_std']
+            data[key] = (data[key] - mean) / std
+
+    def calculate_and_save_normalization_params(self, data):
+        norm_params = {}
+        for key in ['sco2', 'wco2', 'o2', 'psurf_prior', 'geometry', 'y']:
+            mean = torch.mean(data[key])
+            std = torch.std(data[key])
+            data[key] = (data[key] - mean) / std
+            norm_params[f'{key}_mean'] = mean
+            norm_params[f'{key}_std'] = std
+        with open(self.normalize_file, 'wb') as file:
+            pickle.dump(norm_params, file)
+
+    def __len__(self):
         return len(self.y)
 
-    def __getitem__(self, idx) -> Tensor:
-        X = self.X[idx]
-        y = self.y[idx]
+    def __getitem__(self, idx):
+        return {'input': self.X[idx], 'target': self.y[idx]}
+
+class RetrievalDataModule(pl.LightningDataModule):
+    def __init__(self, train_files, test_files, batch_size=32, val_split=0.1, num_workers=4):
+        super().__init__()
+        self.train_files = train_files
+        self.test_files = test_files
+        self.batch_size = batch_size
+        self.val_split = val_split
+        self.num_workers = num_workers
+        self.train_dataset = None
+
+    def setup(self, stage=None):
+        if stage in (None, 'fit'):
+            train_dataset = RetrievalDataSet(self.train_files, train=True)
+            val_size = int(len(train_dataset) * self.val_split)
+            train_size = len(train_dataset) - val_size
+            self.train_dataset, self.val_dataset = random_split(train_dataset, [train_size, val_size])
         
-        sample = {'input': X, 'target': y}
-        return sample
+        if stage in (None, 'test'):
+            self.test_dataset = RetrievalDataSet(self.test_files, train=False)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+    
+    def get_input_shape(self):
+        if self.train_dataset is not None:
+            # Access the dataset to get the shape of the inputs
+            dataset = self.train_dataset.dataset  # Get the actual dataset from the Subset
+            return dataset.X.shape
+        else:
+            raise ValueError("Training dataset is not initialized. Please run setup with stage='fit' first.")
+        
+    def get_target_shape(self):  
+        if self.train_dataset is not None:
+            # Access the dataset to get the shape of the inputs
+            dataset = self.test_dataset.dataset
+            return dataset.y.shape
+        else:   
+            raise ValueError("Training dataset is not initialized. Please run setup with stage='fit' first.")
+
     
 
 class SimDiffDataSet(Dataset):
@@ -526,85 +456,4 @@ class SimDiffDataSet(Dataset):
     
     
 
-
-class RetrievalDataModule(pl.LightningDataModule):
-    """DataModule for OCO Xgas retrieval."""
-    def __init__(self, train_files, val_files, test_files, batch_size=32, use_convolutions = False,  normalize_file='normalize.pkl'):
-        super().__init__()
-        """Args:
-            train_files: The list of training file paths
-            val_files: The list of validation file paths
-            test_files: The list of test file paths
-            batch_size: The batch size for the DataLoader
-            num_workers: The number of workers for the DataLoader
-            use_convolutions: If True, reshape the data to [1, number of samples, number of features]
-            normalize_file: The file to save the normalization parameters
-        """
-        self.train_files = train_files
-        self.val_files = val_files
-        self.test_files = test_files
-        self.batch_size = batch_size
-        self.use_convolutions = use_convolutions
-        self.normalize_file = normalize_file
-
-        self.train_dataset = RetrievalDataSet(self.train_files, train=True, normalize=True, use_convolutions=self.use_convolutions, normalize_file=self.normalize_file)
-        self.X_train, self.Y_train = self.train_dataset.X, self.train_dataset.y
-        self.val_dataset = RetrievalDataSet(self.val_files, train=True, normalize=True, use_convolutions=self.use_convolutions,normalize_file=self.normalize_file)
-        self.X_val, self.Y_val = self.val_dataset.X, self.val_dataset.y
-        self.test_dataset = RetrievalDataSet(self.test_files, train=False, normalize=True, use_convolutions=self.use_convolutions,normalize_file=self.normalize_file)
-        self.X_test, self.Y_test = self.test_dataset.X, self.test_dataset.y
-
-
-    def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True,)
-
-    def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, )
-
-    def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, )
-    
-# TODO : Filter soudnings by outcome_flag; 0 for training, no filtering for training
-class SimDiffDataModule(pl.LightningDataModule):
-    """DataModule for OCO sim - obs differences."""
-    def __init__(self, band, train_files, val_files, test_files, batch_size=32, use_convolutions = False, train_on_sim = False, scaled_eof = False, normalize_file='normalize.pkl'):
-        super().__init__()
-        """Args:
-            band: The band to use X and y. Options are 'sco2', 'wco2', 'o2'
-            train_files: The list of training file paths
-            val_files: The list of validation file paths
-            test_files: The list of test file paths
-            batch_size: The batch size for the DataLoader
-            num_workers: The number of workers for the DataLoader
-            train_on_sim: If True, train on the simulated data
-            use_convolutions: If True, reshape the data to [1, number of samples, number of features]
-            scaled_eof: If True, use scaled the EOFs
-            normalize_file: The file to save the normalization parameters
-        """
-        self.band = band
-        self.train_files = train_files
-        self.val_files = val_files
-        self.test_files = test_files
-        self.batch_size = batch_size
-        self.use_convolutions = use_convolutions
-        self.train_on_sim = train_on_sim
-        self.scaled_eof = scaled_eof
-        self.normalize_file = normalize_file
-
-        self.train_dataset = SimDiffDataSet(self.band, self.train_files, train=True, normalize=True, use_convolutions=self.use_convolutions, train_on_sim = self.train_on_sim,scaled_eof= self.scaled_eof,normalize_file=self.normalize_file)
-        self.X_train, self.Y_train = self.train_dataset.X, self.train_dataset.y
-        self.val_dataset = SimDiffDataSet(self.band, self.val_files, train=True, normalize=True, use_convolutions=self.use_convolutions,train_on_sim = self.train_on_sim,scaled_eof= self.scaled_eof,normalize_file=self.normalize_file)
-        self.X_val, self.Y_val = self.val_dataset.X, self.val_dataset.y
-        self.test_dataset = SimDiffDataSet(self.band, self.test_files, train=False, normalize=True, use_convolutions=self.use_convolutions,train_on_sim = self.train_on_sim,scaled_eof= self.scaled_eof,normalize_file=self.normalize_file)
-        self.X_test, self.Y_test = self.test_dataset.X, self.test_dataset.y
-
-
-    def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True,)
-
-    def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, )
-
-    def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, )
 
