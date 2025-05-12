f05dfb75a0c0457a82fa6b633eec4bbf468cd488
William Keely <williamkeely@gmail.com>
2025-05-12 14:18:14 -0700

    Update wl_alignment.py

--- utils/wl_alignment.py
+++ utils/wl_alignment.py
@@ -1,132 +1,59 @@
 import numpy as np
 import pickle
 import glob
-from tqdm import tqdm
 
-from multiprocessing import Pool
 
 # William Keely william.r.keely [at] jpl.nasa.gov; Steffen Mauceri steffen.mauceri [at] jpl.nasa.gov
 # Script to align the wavelengths of the reflectance simulations and reflectance observations
 
 # TODO : add auto encoder test pickle output
 
-def make_pool_map(years, months, days):
-    if days is None:
-        pool_map = []
-        for year in years:
-            for month in months:
-                pool_map.append((year, month))
-    else:
-        pool_map = []
-        for year in years:
-            for month in months:
-                for day in days:
-                    pool_map.append((year, month, day))
+import numpy as np
+from scipy.interpolate import interp1d
+
+def align_to_grid(data, band_key, wls_key, idxs_key, pixel_lut_range, cut_from_front_back):
+    # Get the band, wavelength, and index data
+    band = data[band_key]
+    band_wl = data[wls_key]
+    band_idxs = data[idxs_key]
     
-    return pool_map
+    # Define pixel lookup table and grid start/stop
+    pixel_lut = np.arange(*pixel_lut_range).astype(int)
+    grid_start_stop = (band_wl[0][0], band_wl[0][-1])
+
+    # Align data to the common grid by inserting missing indices
+    for p in range(len(band)):
+        missing = np.setdiff1d(pixel_lut, band_idxs[p])
+        for m in missing:
+            idx = int(np.where(pixel_lut == m)[0][0])
+            band_idxs[p] = np.insert(band_idxs[p], idx, m)
+            band[p] = np.insert(band[p], idx, np.nan)
+            band_wl[p] = np.insert(band_wl[p], idx, np.nan)
+
+    # Interpolate the bands
+    x = np.linspace(grid_start_stop[0], grid_start_stop[1], band[0].shape[0]).astype(float)
+    band_interp = []
+    for p in range(len(band)):
+        f = interp1d(band_wl[p], band[p], kind="linear", fill_value="extrapolate")
+        band_interp.append(f(x))
+
+    # Cut off the ends as per the provided indices
+    for p in range(len(band_interp)):
+        band_interp[p] = band_interp[p][cut_from_front_back[0]:cut_from_front_back[1]]
+        band_wl[p] = x[cut_from_front_back[0]:cut_from_front_back[1]]
+        band_idxs[p] = band_idxs[p][cut_from_front_back[0]:cut_from_front_back[1]]
+
+        # Update the data dictionary
+        data[band_key][p] = band_interp[p]
+        data[wls_key][p] = band_wl[p]
+        data[idxs_key][p] = band_idxs[p]
 
 
-def main(pool_map, t = 0.0002, nadir = True, debug = True):
-    year = pool_map[0]
-    month = pool_map[1]
-    if len(pool_map) == 3:
-        day = pool_map[2]
-
-
-    if nadir:
-        save_name = './L2DiaND_XCO2_' + str(year) + '_' + str(month) + '_'+ str(day) +'.p'
-        files = glob.glob('./Data/'+str(year)+'/L2DiaND_XCO2_'+str(year)+'_'+str(month)+'_'+str(day)+'_*.p')
-    else:
-        save_name = './L2DiaGL_XCO2_' + str(year) + '_' + str(month) + '_' + str(day) + '.p'
-        files = glob.glob('./Data/'+str(year)+'/L2DiaGL_XCO2_'+str(year)+'_'+str(month)+'_'+str(day)+'_*.p')
-
-    if debug:
-        files = files[:100]
-
-    ref_sim = []
-    ref_obs = []
-    wl = []
-    state = []
-    stat_var = []
-    for p in files:
-        file = pickle.load(open(p, 'rb'))
-        ref_p = file['ref_sim'] #reflectance
-        ref_obs_p = file['ref_obs'] #reflectance observations
-        wl_p = file['wl'] #wavenumber
-        state_p = file['state'] #atmospheric state
-        state_var_p = file['state_var'] #name of state variables
-
-        ref_sim.append(ref_p)
-        ref_obs.append(ref_obs_p)
-        wl.append(wl_p)
-        state.append(state_p)
-        stat_var.append(state_var_p)
-
-    ref_sim = np.vstack(ref_sim)
-    ref_obs = np.vstack(ref_obs)
-    wl = np.vstack(wl)
-    state = np.vstack(state)
-    stat_var = stat_var[0][:4]
-
-    # # remove wavelengths with gaps
-    wl_diff = []
-    for i in tqdm(range(len(wl))):
-        diff = np.diff(wl[i, :])
-
-        wl_diff.append(np.max(diff[diff <= 0.4]))
-    wl_diff = np.array(wl_diff)
-
-    # t is the threshold for removing wavelengths with gaps 
-    ref = ref[wl_diff < t,:]
-    state = state[wl_diff < t,:]
-    wl = wl[wl_diff < t,:]
-
-    # find common wavelengths grid
-    wl_med = np.median(wl[::5,:],0)
-
-    # interpolate wavelengths on common wavelength grid
-    ref_meds_sim = []
-    ref_meds_obs =[]
-    for i in tqdm(range(len(wl))):
-        ref_s = np.interp(wl_med, wl[i,:],ref[i,:], left=0, right=0)
-        ref_meds_sim.append(ref_s)
-        ref_o = np.interp(wl_med, wl[i,:],ref_obs[i,:], left=0, right=0)
-        ref_meds_obs.append(ref_o)
-        
-    ref_med_sim = np.vstack(ref_meds_sim)
-    ref_med_obs = np.vstack(ref_meds_obs)
-
-    # remove columns with zeros
-    ref_med_sim = ref_med_sim[:,1:-10]
-    ref_med_obs = ref_med_obs[:,1:-10]
-    wl_med = wl_med[1:-10]
-
-    # remove rows with zeros
-    state = state[np.min(ref_med_sim,1)>0,:]
-    ref_med_sim = ref_med_sim[np.min(ref_med_sim,1)>0,:]
-    ref_med_obs = ref_med_obs[np.min(ref_med_obs,1)>0,:]
-
-    # change units to something more managable
-    # state[:,0] = state[:,0]*10**6
-
-    print(str(len(ref_med_sim)) + ' soundings')
-
-    #save everything as pickle file
-    save_data = {'ref_sim': ref_med_sim,'ref_obs' : ref_med_obs , 'wl':wl_med, 'state': state, 'state_var': stat_var}
-    pickle.dump( save_data, open( save_name, "wb" ), protocol = 4)
-    print('done')
-
-
-
-if __name__ == '__main__':
-    years = ['2015'] # just run one year at a time
-    months = ['01','02','03','04','05','06','07','08','09','10','11','12']
-    days = ['01','02','03','04','05','06','07','08','09','10',
-            '11','12','13','14','15','16','17','18','19','20',
-            '21','22','23','24','25','26','27','28','29','30','31']
-    pool_map = make_pool_map(years, months, days)
-    with Pool(1) as p:
-        p.map(main, pool_map)
+""" Example!!! """
+# Now you can call this function for each band with the corresponding parameters
+# align_to_grid(data, "o2_band", "o2_wls", "o2_idxs", (91, 950), (10, -129))
+# align_to_grid(data, "wco2_band", "wco2_wls", "wco2_idxs", (211, 900), (20, -157))
+# align_to_grid(data, "sco2_band", "sco2_wls", "sco2_idxs", (100, 950), (70, -148))
 
 
 
